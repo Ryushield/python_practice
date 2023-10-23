36c306518e70c8b33c4c8acd8ef6cd63e3b8439c
Ryu Shield <change121@naver.com>
2023-10-23 16:12:39 +0900

    first commit

---
+++ DemoSetTuple.py
@@ -0,0 +1,20 @@
+#DemoSetTuple.py
+
+a={1,2,3,3}
+b={3,4,4,5}
+
+print(a)
+print(b)
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+
+
+
+print("-----tuple형식------")
+tp=(1,2,3)
+print(tp)
+print(len(tp))
+print(tp[0])
+
+print("id : %s", "name : %s" % "kim","김유신")
