43fadcb601f4c4e75a986ccc6e5ff7d23ccec05c
Ryu Shield <change121@naver.com>
2023-10-23 16:30:54 +0900

    두번쨰 함수 연습

--- func1.py
+++ func1.py
@@ -1,10 +1,18 @@
 #func1.py
 #함수정의
 
-def setValue(newValue)
+def setValue(newValue):
     x=newValue
     print(x)
 
 
 #함수 호출
 setValue(5)
+
+
+#값을 리턴하는 함수
+def swap(x,y):
+    return y,x
+
+result = swap(5,6)
+print(result)
